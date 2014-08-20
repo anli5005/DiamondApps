5bbc7a63a2cceb9c474e65c8e52c553f3cf1a178
anli5005 <li.anthony917@gmail.com>
2014-08-20 09:16:26 -0400

    Update edit launcher

--- Edit-Launcher.app/main.lua
+++ Edit-Launcher.app/main.lua
@@ -1,3 +1,21 @@
 term.setBackgroundColor(colors.black)
 term.setTextColor(colors.white)
 term.setCursorPos(1,2)
+local args = {...}
+shell.setDir("")
+if args[1] then
+  if string.sub(args[1], 1, 1) == "/" then
+    shell.run("edit", args[1])
+  else
+    shell.run("edit", fs.combine("/Users/"..DLogged, args[1]))
+  end
+else
+  print(" Welcome to Edit!")
+  print(" Please type in the name of your file:")
+  local f = read()
+  if string.sub(f, 1, 1) == "/" then
+    shell.run("edit", f)
+  else
+    shell.run("edit", fs.combine("/Users/"..DLogged, args[1]))
+  end
+end
