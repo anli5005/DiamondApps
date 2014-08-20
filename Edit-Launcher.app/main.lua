6a0768f3917ba65cd2c7b3ab379adef05be972f2
anli5005 <li.anthony917@gmail.com>
2014-08-20 10:33:50 -0400

    Fix edit bug

--- Edit-Launcher.app/main.lua
+++ Edit-Launcher.app/main.lua
@@ -5,17 +5,17 @@ local args = {...}
 shell.setDir("")
 if args[1] then
   if string.sub(args[1], 1, 1) == "/" then
-    shell.run("edit", args[1])
+    shell.run("/rom/programs/edit", args[1])
   else
-    shell.run("edit", fs.combine("/Users/"..DLogged, args[1]))
+    shell.run("/rom/programs/edit", fs.combine("/Users/"..DLogged, args[1]))
   end
 else
   print(" Welcome to Edit!")
   print(" Please type in the name of your file:")
   local f = read()
   if string.sub(f, 1, 1) == "/" then
-    shell.run("edit", f)
+    shell.run("/rom/programs/edit", f)
   else
-    shell.run("edit", fs.combine("/Users/"..DLogged, f))
+    shell.run("/rom/programs/edit", fs.combine("/Users/"..DLogged, f))
   end
 end
